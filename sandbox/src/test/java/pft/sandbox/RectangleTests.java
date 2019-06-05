bdf77a58a451a7beb968d1fdc31c350797f75c96
Anton <trudovanton1@gmail.com>
2019-06-05 23:34:35 +0300

    simple tests for Square, Rect.

---
+++ sandbox/src/test/java/pft/sandbox/RectangleTests.java
@@ -0,0 +1,14 @@
+package pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RectangleTests {
+
+    @Test
+    public void testArea(){
+        Rectangle rectangle = new Rectangle(4,5);
+        Assert.assertEquals(rectangle.area(), 20.0);
+    }
+
+}
