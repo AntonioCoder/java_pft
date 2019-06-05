cfea85a0e005e6b5798e268ce209031ba0b61604
Anton <trudovanton1@gmail.com>
2019-06-05 23:07:16 +0300

    classes, methods

--- sandbox/src/main/java/pft/sandbox/MyFirstProgramme.java
+++ sandbox/src/main/java/pft/sandbox/MyFirstProgramme.java
@@ -3,17 +3,17 @@ package pft.sandbox;
 public class MyFirstProgramme {
     public static void main(String[] args) {
         hello("Anton");
-        System.out.println("S kvadrat "+area(5));
-        System.out.println("S priamo " +area(7,8));
+
+        Square square = new Square(5);
+        System.out.println("S kvadrat "+square.area());
+
+        Rectangle rectangle = new Rectangle(6,7);
+        System.out.println("S priamo " +rectangle.area());
     }
 
     public static void hello(String user) {
         System.out.println("Helloworld "+user);
     }
-    public static double area(double l){
-        return l*l;
-    }
-    public static double area(double a, double b){
-        return a*b;
-    }
+
+
 }
