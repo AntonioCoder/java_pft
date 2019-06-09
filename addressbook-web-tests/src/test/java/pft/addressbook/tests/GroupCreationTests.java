850ba3053947a81b60d45db5a83b9dd2c84b55db
Anton <trudovanton1@gmail.com>
2019-06-10 02:30:53 +0300

    Created amazing architecture

---
+++ addressbook-web-tests/src/test/java/pft/addressbook/tests/GroupCreationTests.java
@@ -0,0 +1,23 @@
+package pft.addressbook.tests;
+
+
+import org.testng.annotations.*;
+import pft.addressbook.model.GroupData;
+
+
+public class GroupCreationTests extends TestBase{
+
+
+  @Test
+  public void testGroupCreation() throws Exception {
+
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().initGroupCreation();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitGroupCreation();
+    app.getGroupHelper().returnGroupPage();
+    app.getSessionHelper().logout();
+  }
+
+
+}
---
+++ addressbook-web-tests/src/test/java/pft/addressbook/tests/GroupDeletingTest.java
@@ -0,0 +1,17 @@
+package pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletingTest extends TestBase {
+
+
+    @Test
+    public void testGroupDeleting() throws Exception {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().deleteSelectedGroups();
+        app.getGroupHelper().returnGroupPage();
+    }
+
+
+}
