2cf136af12b0de96876397b8355a9c2bdc62cfdc
Anton <trudovanton1@gmail.com>
2019-06-18 02:20:09 +0300

    Added interactions with collections
    Now there're few additional checkings, in Group Creation, Modification and Deleting tests there're
    comparison of collections, not just sizes

--- addressbook-web-tests/src/test/java/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pft/addressbook/tests/GroupCreationTests.java
@@ -1,8 +1,12 @@
 package pft.addressbook.tests;
 
 
-import org.testng.annotations.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import pft.addressbook.model.GroupData;
+
+import java.util.HashSet;
+import java.util.List;
 
 
 public class GroupCreationTests extends TestBase{
@@ -12,7 +16,16 @@ public class GroupCreationTests extends TestBase{
   public void testGroupCreation() {
 
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    GroupData group = (new GroupData( "test1", "test2", "test3"));
+    app.getGroupHelper().createGroup(group);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() + 1);
+
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    before.add(group);
+    Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
   }
 
 
--- addressbook-web-tests/src/test/java/pft/addressbook/tests/GroupDeletingTests.java
+++ addressbook-web-tests/src/test/java/pft/addressbook/tests/GroupDeletingTests.java
@@ -1,20 +1,30 @@
 package pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pft.addressbook.model.GroupData;
+
+import java.util.List;
 
 public class GroupDeletingTests extends TestBase {
 
 
     @Test
     public void testGroupDeleting(){
+
         app.getNavigationHelper().gotoGroupPage();
         if (!app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
         }
-        app.getGroupHelper().selectGroup();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size()-1);
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnGroupPage();
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after);
     }
 
 
